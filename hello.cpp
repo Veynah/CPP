aef1d64934504c8d4ab05ccc6e2a73c2dddc4799
AdamTangiev <tangiev96@gmail.com>
2023-07-15 15:34:39 +0200

    c

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Hello world!" << std::endl ;
+int main()
+{
+    std::cout << "Hello world! \n" << std::endl;
+    std::cout << "How are you ?" << std::endl;
     return 0;
 }
